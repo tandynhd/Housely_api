d5c20e6898985dcb2ed6bedb45b519b5967de2ca
Tandin Dorji <6222790428@g.siit.tu.ac.th>
2021-10-07 23:08:57 +0700

    test3

--- library/catalog.php
+++ library/catalog.php
@@ -61,7 +61,7 @@ else{?>
                 <div class="banner-header">
                     <h2>Services</h2>
                     <span class="underline center"></span>
-                    <p class="lead">"Getting a house isn't the hard part, maintaining it is!!"</p>
+                    <p class="lead">"Getting a house isn't the hard part, getting a soul mate is."</p>
                 </div>
                 <div class="breadcrumb">
                     <ul>
