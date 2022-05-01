af9cf1cba916dd31b848f742da92079cef78c414
tandynhd <6222790428@g.siit.tu.ac.th>
2022-05-02 00:12:43 +0700

    keep

--- library/index.php
+++ library/index.php
@@ -39,7 +39,7 @@ echo "<script>alert('Invalid Details');</script>";
         <!-- Meta -->
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
-        <title> Housely mto</title>
+        <title> Housely </title>
         <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i%7CLato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
